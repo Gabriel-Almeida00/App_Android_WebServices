9b26906ca7269850b1b28bb548a38fc3eae917db
gabriel <gaamdal557@gmail.com>
2023-04-15 03:40:55 -0300

    Tarefa assíncrona

--- app/src/main/java/com/example/threads/MainActivity.java
+++ app/src/main/java/com/example/threads/MainActivity.java
@@ -4,86 +4,35 @@ import androidx.annotation.LongDef;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.ipsec.ike.exceptions.IkeInternalException;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnIniciar;
-    private int numero;
-    //private Handler handler = new Handler();
-    private boolean pararExecucao = false;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnIniciar = findViewById(R.id.btnIniciar);
+        progressBar = findViewById(R.id.progressBar);
     }
 
-    public void inicarThread(View v){
-       /* MyThread thread = new MyThread();
-        thread.start();*/
+    public void IniciarTaskAsync(View v){
 
-        pararExecucao = false;
-        MyRunnable runnable = new MyRunnable();
-        new Thread(runnable).start();
-    }
-    public void pararThread(View v){
-        pararExecucao = true;
     }
 
-    class MyRunnable implements Runnable{
+    class MyAsyncTask extends AsyncTask<Integer,Integer,String >{
         @Override
-        public void run() {
-            for(int i=0;i<15;i++){
-
-                if(pararExecucao)
-                    return;
-
-                numero = i;
-                Log.d("Thread","contador: "+i);
-
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        btnIniciar.setText("Contador: "+numero);
-                    }
-                });
-
-                /*
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        btnIniciar.setText("Contador: "+numero);
-                    }
-                });*/
-
-                try{
-                    Thread.sleep(1000);
-                }catch (InterruptedException ex){
-                    ex.printStackTrace();
-                }
-            }
+        protected String doInBackground(Integer... integers) {
+            return null;
         }
     }
 
-    class MyThread extends Thread{
-        @Override
-        public void run() {
-            for(int i=0;i<15;i++){
-                Log.d("Thread","contador: "+i);
-                try{
-                    Thread.sleep(1000);
-                }catch (InterruptedException ex){
-                    ex.printStackTrace();
-                }
-            }
-        }
-    }
 }
