bf0c30143e7b4e53e0f05a4723c35f63629fbd60
gabriel <gaamdal557@gmail.com>
2023-04-18 17:03:43 -0300

    processando json

--- app/src/main/java/com/example/threads/MainActivity.java
+++ app/src/main/java/com/example/threads/MainActivity.java
@@ -1,18 +1,15 @@
 package com.example.threads;
 
-import androidx.annotation.LongDef;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.ipsec.ike.exceptions.IkeInternalException;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -22,71 +19,114 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.net.ssl.HttpsURLConnection;
-
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnRecuperar;
-    private TextView txtDados;
+    private Button botaoRecuperar;
+    private TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnRecuperar = findViewById(R.id.btnRecuperar);
-        txtDados = findViewById(R.id.txtResultado);
+        botaoRecuperar = findViewById(R.id.btnRecuperar);
+        textoResultado = findViewById(R.id.txtResultado);
 
-        btnRecuperar.setOnClickListener(new View.OnClickListener() {
+        botaoRecuperar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MyTask task = new MyTask();
                 String urlApi = "https://blockchain.info/ticker";
-                String cep ="01310100";
+                String cep = "01310100";
                 String urlCep = "https://viacep.com.br/ws/" + cep + "/json/";
                 task.execute(urlCep);
             }
         });
+
     }
 
-    class MyTask extends AsyncTask<String, Void, String> {
+    class MyTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
 
         @Override
         protected String doInBackground(String... strings) {
+
             String stringUrl = strings[0];
             InputStream inputStream = null;
             InputStreamReader inputStreamReader = null;
             StringBuffer buffer = null;
 
             try {
+
                 URL url = new URL(stringUrl);
-                HttpURLConnection conexao = (HttpsURLConnection) url.openConnection();
+                HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
 
+                // Recupera os dados em Bytes
                 inputStream = conexao.getInputStream();
-                inputStreamReader = new InputStreamReader(inputStream);
 
-                BufferedReader reader = new BufferedReader(inputStreamReader);
+                //inputStreamReader lê os dados em Bytes e decodifica para caracteres
+                inputStreamReader = new InputStreamReader( inputStream );
+
+                //Objeto utilizado para leitura dos caracteres do InpuStreamReader
+                BufferedReader reader = new BufferedReader( inputStreamReader );
                 buffer = new StringBuffer();
-
                 String linha = "";
-                reader.readLine();
 
                 while((linha = reader.readLine()) != null){
-                    buffer.append(linha);
+                    buffer.append( linha );
                 }
 
             } catch (MalformedURLException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
+
             return buffer.toString();
         }
 
         @Override
         protected void onPostExecute(String resultado) {
             super.onPostExecute(resultado);
-            txtDados.setText(resultado);
+
+            String logradouro = null;
+            String cep = null;
+            String complemento = null;
+            String bairro = null;
+            String localidade = null;
+            String uf = null;
+
+           /* String objetoValor = null;
+            String valorMoeda = null;
+            String simbolo = null;*/
+
+            try {
+
+                JSONObject jsonObject = new JSONObject(resultado);
+                logradouro = jsonObject.getString("logradouro");
+                cep = jsonObject.getString("cep");
+                complemento = jsonObject.getString("complemento");
+                bairro = jsonObject.getString("bairro");
+                localidade = jsonObject.getString("localidade");
+                uf = jsonObject.getString("uf");
+
+                /*JSONObject jsonObject = new JSONObject(resultado);
+                objetoValor = jsonObject.getString("BRL");
+
+                JSONObject jsonObjectReal = new JSONObject(objetoValor);
+                valorMoeda = jsonObjectReal.getString("last");
+                simbolo = jsonObjectReal.getString("symbol");*/
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            textoResultado.setText(logradouro+" / "+cep+" / "+complemento+" / "+bairro+" / "+localidade+" / "+uf);
+            //textoResultado.setText(valorMoeda+" /"+simbolo);
         }
     }
 }
